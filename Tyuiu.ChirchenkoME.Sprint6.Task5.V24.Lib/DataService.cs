79df089d431a91507048341905f37482c0262f4d
Vlad <Vlad@VLAD_X>
2025-12-01 23:44:06 +0500

    Task55

--- Tyuiu.ChirchenkoME.Sprint6.Task5.V24.Lib/DataService.cs
+++ Tyuiu.ChirchenkoME.Sprint6.Task5.V24.Lib/DataService.cs
@@ -1,4 +1,6 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.ChirchenkoME.Sprint6.Task5.V24.Lib
 {
     public class DataService : ISprint6Task5V24
@@ -8,19 +10,20 @@ namespace Tyuiu.ChirchenkoME.Sprint6.Task5.V24.Lib
             //Прочитать данные из файла InPutFileTask5V24.txt. Вывести в dataGridView.Дан список из чисел. Вывести все элементы, равные 0. Построить диаграмму по этим значениям. Графический интерфейс пользователя оформить по примеру из лекции. У вещественных значений округлить до трёх знаков после запятой.
             string[] lines = System.IO.File.ReadAllLines(path);
 
-            double[] numbers = new double[lines.Length];
-            for (int i = 0; i < lines.Length; i++)
+            var zeros = new List<double>();
+            foreach (var line in lines)
             {
-                if (double.TryParse(lines[i], out double value))
+                if (double.TryParse(line, NumberStyles.Float | NumberStyles.AllowThousands, CultureInfo.InvariantCulture, out double value))
                 {
-                    numbers[i] = Math.Round(value, 3);
+                    double rounded = System.Math.Round(value, 3);
+                    if (rounded == 0.0)
+                    {
+                        zeros.Add(rounded);
+                    }
                 }
-                else
-                {
-                    numbers[i] = 0; // или обработать ошибку по-другому
-                }
+                // skip non-numeric lines (do not treat them as 0)
             }
-            return numbers;
+            return zeros.ToArray();
         }
     }
 }
