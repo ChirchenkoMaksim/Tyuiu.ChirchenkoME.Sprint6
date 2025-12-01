05b2e051e5b2e3225dcf3b52f73059e1d8113c37
Vlad <Vlad@VLAD_X>
2025-12-01 23:42:42 +0500

    Task5

---
+++ Tyuiu.ChirchenkoME.Sprint6.Task5.V24.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.ChirchenkoME.Sprint6.Task5.V24.Lib
+{
+    public class DataService : ISprint6Task5V24
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            //Прочитать данные из файла InPutFileTask5V24.txt. Вывести в dataGridView.Дан список из чисел. Вывести все элементы, равные 0. Построить диаграмму по этим значениям. Графический интерфейс пользователя оформить по примеру из лекции. У вещественных значений округлить до трёх знаков после запятой.
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            double[] numbers = new double[lines.Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (double.TryParse(lines[i], out double value))
+                {
+                    numbers[i] = Math.Round(value, 3);
+                }
+                else
+                {
+                    numbers[i] = 0; // или обработать ошибку по-другому
+                }
+            }
+            return numbers;
+        }
+    }
+}
---
+++ Tyuiu.ChirchenkoME.Sprint6.Task5.V24.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ChirchenkoME.Sprint6.Task5.V24.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
