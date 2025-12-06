67b771ff7926022a5c44c86b9069d8702b04b7af
Vlad <Vlad@VLAD_X>
2025-12-06 23:26:07 +0500

    Tas77

--- Tyuiu.ChirchenkoME.Sprint6.Task7.V3.Lib/DataService.cs
+++ Tyuiu.ChirchenkoME.Sprint6.Task7.V3.Lib/DataService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.ChirchenkoME.Sprint6.Task7.V3.Lib
 {
@@ -7,7 +9,7 @@ namespace Tyuiu.ChirchenkoME.Sprint6.Task7.V3.Lib
         public int[,] GetMatrix(string path)
         {
             //Дан файл InPutFileTask7V3.csv в котором хранится матрица целочисленных значений. Загрузить файл через openFileDialog в объект dataGridViewIn. Изменить в первом столбце четные значения на -1 . Результат вывести в объект dataGridViewOut. Сохранить результат в файл OutPutFileTask7.csv через saveFileDialog. Графический интерфейс пользователя оформить по образцу как в лекции
-            string[] lines = System.IO.File.ReadAllLines(path);
+            string[] lines = File.ReadAllLines(path);
             if (lines.Length == 0)
                 return new int[0, 0];
 
@@ -43,14 +45,54 @@ namespace Tyuiu.ChirchenkoME.Sprint6.Task7.V3.Lib
                     }
                 }
             }
-            for (int i = 0; i < rowCount; i++)
+
+            // Do not modify matrix here; processing is performed by ProcessMatrix
+            return matrix;
+        }
+
+        public int[,] ProcessMatrix(int[,] input)
+        {
+            if (input == null)
+                return new int[0, 0];
+
+            int rows = input.GetLength(0);
+            int cols = input.GetLength(1);
+            int[,] result = new int[rows, cols];
+            for (int i = 0; i < rows; i++)
             {
-                if (matrix[i, 0] % 2 == 0)
+                for (int j = 0; j < cols; j++)
                 {
-                    matrix[i, 0] = -1;
+                    result[i, j] = input[i, j];
+                }
+                // change even values in first column to -1
+                if (cols > 0)
+                {
+                    int v = result[i, 0];
+                    if (v % 2 == 0)
+                        result[i, 0] = -1;
                 }
             }
-            return matrix;
+            return result;
+        }
+
+        public void SaveToFile(string path, int[,] matrix)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            var sb = new StringBuilder();
+            for (int i = 0; i < rows; i++)
+            {
+                var parts = new string[cols];
+                for (int j = 0; j < cols; j++)
+                {
+                    parts[j] = matrix[i, j].ToString();
+                }
+                sb.AppendLine(string.Join(";", parts));
+            }
+            File.WriteAllText(path, sb.ToString());
         }
     }
 }
--- Tyuiu.ChirchenkoME.Sprint6.Task7.V3/Form1.Designer.cs
+++ Tyuiu.ChirchenkoME.Sprint6.Task7.V3/Form1.Designer.cs
@@ -29,11 +29,127 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            groupBox1 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            toolTip1 = new ToolTip(components);
+            panel1 = new Panel();
+            panel2 = new Panel();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 1);
+            button1.Name = "button1";
+            button1.Size = new Size(77, 60);
+            button1.TabIndex = 0;
+            button1.Text = "Загрузить";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(83, 1);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 60);
+            button2.TabIndex = 1;
+            button2.Text = "Сохранить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(164, 1);
+            button3.Name = "button3";
+            button3.Size = new Size(80, 60);
+            button3.TabIndex = 2;
+            button3.Text = "Выполнить";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(3, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(794, 70);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = resources.GetString("groupBox1.Text");
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(0, 149);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(392, 289);
+            dataGridView1.TabIndex = 4;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(416, 149);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(384, 289);
+            dataGridView2.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(groupBox1);
+            panel1.Location = new Point(0, 67);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 76);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(398, 149);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(10, 198);
+            panel2.TabIndex = 6;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private ToolTip toolTip1;
+        private Panel panel1;
+        private Panel panel2;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
     }
 }
